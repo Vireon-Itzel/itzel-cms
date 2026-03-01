ad94aa04f62623865353a96ab23884bbb79861aa
PazMedinaGuz <pazmedinaupn@gmail.com>
2026-03-01 15:18:50 -0600

    feat(single-type): add legal single type with repeatable contentLegal

---
+++ src/api/legal-content/controllers/legal-content.ts
@@ -0,0 +1,7 @@
+/**
+ * legal-content controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::legal-content.legal-content');
---
+++ src/api/legal-content/services/legal-content.ts
@@ -0,0 +1,7 @@
+/**
+ * legal-content service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::legal-content.legal-content');
